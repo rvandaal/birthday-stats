763a99ed70bceec9afb9558bd13fdf79e0f78099
Rob van Daal <rvandaal@hotmail.com>
2018-08-19 07:32:26 +0200

    changed yearlist in datepicker

--- src/app/input-date/input-date.component.ts
+++ src/app/input-date/input-date.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-input-date',
   templateUrl: './input-date.component.html',
-  styleUrls: ['./input-date.component.css']
+  styleUrls: ['./input-date.component.css'],
+  providers: [NgbDatepickerConfig]
 })
 export class InputDateComponent implements OnInit {
 
   @Input()
   personGroup: FormGroup;
 
-  constructor() { }
+  constructor(config: NgbDatepickerConfig) {
+    config.minDate = {year: 1900, month: 1, day: 1};
+    config.maxDate = {year: moment().year() + 1, month: 1, day: 1};
+    config.outsideDays = 'hidden';
+  }
 
   ngOnInit() {
   }
